8c903e80e7ccae1f08e5da376611a47a584082f6
acel5o <56363014+acel5o@users.noreply.github.com>
2020-11-02 20:29:15 +0700

    Ubah API

--- app.js
+++ app.js
@@ -16,17 +16,20 @@ let users = [
     {
         "username" : "admin",
         "name" : "admin",
+        "password" : "123",
         "role" : "admin"
     },
     {
         "username" : "admin1",
         "name" : "admin1",
+        "password" : "123",
         "role" : "admin"
     },
     {
-        "username" : "admin2",
-        "name" : "admin2",
-        "role" : "admin"
+        "username" : "acel5o",
+        "name" : "Zefa",
+        "password" : "123",
+        "role" : "user"
     },
 ]
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http")
 const url = require("url")
 const app = require("./app")
 const hostname = "127.0.0.1"
-const port = 3001
+const port = 3001   
 
 const server = http.createServer(app)
 // const server = http.createServer((req,res) => {
