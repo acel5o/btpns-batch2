56a93a540e795747d534b132fa488aa48eeada94
acel5o <56363014+acel5o@users.noreply.github.com>
2020-11-05 10:45:06 +0700

    NODE API Pake DB

--- src/models/users.js
+++ src/models/users.js
@@ -1,22 +1,42 @@
-let users = [
-        {
-            "username" : "admin",
-            "name" : "admin",
-            "password" : "123",
-            "role" : "admin"
-        },
-        {
-            "username" : "admin1",
-            "name" : "admin1",
-            "password" : "123",
-            "role" : "admin"
-        },
-        {
-            "username" : "acel5o",
-            "name" : "Zefa",
-            "password" : "123",
-            "role" : "user"
-    }
-]
+const conn = require("../confiq/database")
 
-module.exports = users
+const getUser=(username=null,password=null,cb=()=>{})=>{
+    try{
+        // const query = `select * from user1 where username='${username}' and password='${password}'`
+        conn.query("select role from user1 where username=? and password=? limit 1", [username,password],
+        function(error,results, fields){
+            if (error) {
+                console.log("ERROR : getUser -", + error);
+                return cb("Internal Server Error!!", null)
+            }
+            return cb(null, results)
+          });
+
+    }catch(err){
+        console.log("ERROR getUser.catch", + error);
+        return cb("Internal Server Error!!", null)
+    }
+}
+// let users = [
+//         {
+//             "username" : "admin",
+//             "name" : "admin",
+//             "password" : "123",
+//             "role" : "admin"
+//         },
+//         {
+//             "username" : "admin1",
+//             "name" : "admin1",
+//             "password" : "123",
+//             "role" : "admin"
+//         },
+//         {
+//             "username" : "acel5o",
+//             "name" : "Zefa",
+//             "password" : "123",
+//             "role" : "user"
+//     }
+// ]
+
+
+module.exports = {getUser}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,8 +2,8 @@ const express = require("express")
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
-const jwtAuth = require("../middleware/jwtAuth")
-const users = require("../models/users")
+// const jwtAuth = require("../middleware/jwtAuth")
+const userModel = require("../models/users")
 
 const router = express.Router()
 const key = process.env.JWT_KEY
@@ -11,26 +11,31 @@ const key = process.env.JWT_KEY
 router.post("/login", (req, res) => {
     const { username, password } = req.body
 
-    const userLogin = users.find(user => user.username === username && user.password === password)
+    // const userLogin = users.find(user => user.username === username && user.password === password)
     // login success
-    if (userLogin) {
-        const dataUser = {
-            username,
-            role: userLogin.role
+
+    userModel.getUser(username, password, (error,data)=>{
+        if(error) return res.status(500).send({error})
+
+        if (data.length) {
+            const dataUser = {
+                username,
+                role: data[0].role
+            }
+    
+            // create jwt token
+            const token = jwt.sign(dataUser, key, { expiresIn: '1h' })
+    
+            return res.status(200).send({
+                message: "User found!!",
+                data: [{token }]
+            })
         }
-
-        // create jwt token
-        const token = jwt.sign(dataUser, key, { expiresIn: '1h' })
-
-        return res.status(200).send({
-            message: "User found!!",
-            data: [{ dataUser,token }]
+    
+        // login failed
+        return res.status(401).send({
+            error: "User not found!!"
         })
-    }
-
-    // login failed
-    return res.status(401).send({
-        error: "User not found!!"
     })
 })
 
@@ -43,7 +48,5 @@ router.post("/register", (req, res)=>{
         message: "POST Success"
     })
 })
-
-
 
 module.exports = router
